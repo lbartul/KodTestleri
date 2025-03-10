a69121ccfbf01e29b1d8f46beb0c2a6b65b4b12a
lbartul <lbartuaksul@gmail.com>
2025-03-10 18:07:39 +0300

    Login ve Success yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min'
 import './App.css'
+import Login from './Components/Login'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/main">
+            <h1>Main Page</h1>
+          </Route>
+          <Route exact path="/error">
+          <h1>Error Page</h1>
+          </Route>
+        </Switch>
   )
 }
 
